66602db44b73d9d4c2b1f07f9596fd644603af7d
core.pager=cat <benzaouikeltoum724@gmail.com>
2022-09-06 15:10:09 +0100

    finish JS Part

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const   qst=[
 
 {Question:'What is Java Script :',
  a: 'Programming Language',
- b:'Script Languag',
+ b:'Script Language',
  c:'Both',
  d:'other Thing',
- correct: 'a'
+ correct: 'Programming Language'
 
 },
 {
@@ -14,7 +14,7 @@ a: "Hyper Text Modele Lnaguage",
 b:"Home Text Modele Lnaguage",
 c:"Hyper Text Markup Language",
 d:"Hyper title  Markup Language",
-correct: 'c'
+correct:"Hyper Text Markup Language"
 
 },
 {
@@ -23,34 +23,134 @@ a:"Mercury",
 b:"Mars",
 c:"Uranus",
 d:"Neptune",
-correct: 'a'
+correct:"Mercury"
+
+},
+{
+Question :" Which African country was formerly known as Abyssinia?",
+a:"Niger",
+b:"Mali",
+c:"Ethiopia ",
+d:"Algeria",
+correct:"Ethiopia "
+
+},
+{
+Question :"According to Greek mythology, who was the first woman on earth?",
+a:"Pandora",
+b:"Meriem",
+c:"Hawa",
+d:"Sarah",
+correct:"Pandora"
+
+},
+{
+Question :"Which country consumes the most chocolate per capita?",
+a:"France",
+b:"USA",
+c:"Switzerland",
+d:"Spain",
+correct:"Switzerland"
+
+},
+{
+Question :"Which two U.S. states don’t observe Daylight Saving Time?",
+a:"Arizona and Hawaii",
+b:"Florida and virginia",
+c:"Texas and Hawaii",
+d:"California and Georgia",
+correct:"Arizona and Hawaii"
+
+},
+{
+Question :"Who was the first female Prime Minister of Great Britain?",
+a:"Margaret Thatcher",
+b:"Isabel Perón",
+c:"Sirimavo Bandaranaike",
+d:"Indira Gandhi",
+correct:"Margaret Thatcher"
 
 }
 ]
 
+let loadQuestions=()=>{
+// labels
+const qu=document.getElementById('h');
+const la =document.getElementById('boxa');
+const lb =document.getElementById('boxb');
+const lc =document.getElementById('boxc');
+const ld =document.getElementById('boxd');
+//pick a random number between 0 and 3
+var i=Math.floor(Math.random() * (2 + 1));
 
-console.log(qst[0].Question);
+//set the question in labels
 
-const ad =document.getElementById('box');
-console.log(ad)
-ad.innerText="helloooooooooooo"
+qu.innerText=qst[i].Question;
+la.innerText=qst[i].a;
+lb.innerText=qst[i].b;
+lc.innerText=qst[i].c;
+ld.innerText=qst[i].d; 
+return i;
+}
+var i=loadQuestions();
 
-console.log(document.getElementById('box'))
-var parentDOM = document.getElementsByClassName('checkmarka');
-console.log(parentDOM.namedItem)
-var testElements = document.getElementsByClassName('checkmarka');
-var testDivs = Array.prototype.filter.call(testElements, function(testElement){
-    return testElement.nodeName === 'div';
-});
+//test if the correct answer is checked
+function testChecked(){
+if (document.getElementById('ia').checked){
+     var elea = document.getElementById('boxa');
+     //içi je vais tester la avec qst[i]
+     if(qst[i].correct==elea.innerHTML)
+     {
+alert(elea.innerHTML+" is correct");
+i=loadQuestions();
+    
+}else
+{
+alert(elea.innerHTML+" is incorrect");
+i=loadQuestions();
+}
 
-console.log(testDivs)
+}
+if (document.getElementById('ib').checked){
+    var eleb = document.getElementById('boxb');
+    //içi je vais tester la with qst[i]
+    if(qst[i].correct==eleb.innerHTML)
+    {
+alert(eleb.innerHTML+" is correct");
 
+i=loadQuestions();
+}
+else {
+alert(eleb.innerHTML+" is incorrect");}
+i=loadQuestions();
+}
+if (document.getElementById('ic').checked){
+    var elec = document.getElementById('boxc');
+    //içi je vais tester la with qst[i]
+    if(qst[i].correct==elec.innerHTML)
+    {
+alert(elec.innerHTML+" is correct");
 
-/*
+i=loadQuestions();
 
-er.a=qst[0].a
+   
+}else
+{
+alert(elec.innerHTML+" is incorrect");
+i=loadQuestions();
+}}
 
-
-
-
-/*42 SECOND */
+if (document.getElementById('id').checked){
+    var eled = document.getElementById('boxd');
+    //içi je vais tester la with qst[i]
+    if(qst[i].correct==eled.innerHTML)
+    {
+alert(eled.innerHTML+" is correct");
+i=loadQuestions();
+   
+}
+else{
+alert(eled.innerHTML+" is incorrect");
+i=loadQuestions();
+}}
+}
